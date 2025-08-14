b1b5046019dcebe82f4e9fff82a553d51d504039
Павел Новопашин <95532839+MolderrOff@users.noreply.github.com>
2025-08-14 22:50:50 +0500

    Update IBaseRepository.cs
    Del comments

--- Automarket.DAL/Interfaces/IBaseRepository.cs
+++ Automarket.DAL/Interfaces/IBaseRepository.cs
@@ -3,16 +3,15 @@ using System.Threading.Tasks;
 
 namespace Itmytask.DAL.Interfaces
 {
-    public interface IBaseRepository<T> //Общие методы для взаимодействия с бд <> - значит Generic
-    {                                     //Generic -система понимает с каким объектом имеет дело при передаче
-                                          //при наследовании интерфейса передаётся определённый объект
-        Task<bool> Create(T entity);// bool Create(T entity);
-        Task<T> GetAsync(int id);  //метод 2   T GetAsync(int id); 
+    public interface IBaseRepository<T> 
+    {                                     
+        Task<bool> Create(T entity);
+        Task<T> GetAsync(int id);  
         //IEnumerable<T> Select();
-        Task<List<T>> Select(); //  получение коллекции элементов в тип Generic передаём селект без параметров //IEnumerable<T> Select();
+        Task<List<T>> Select(); 
         Task<List<T>> GetAsyncSelect();
 
-        Task<bool> Delete(T entity); //4 метод удаления из таблицы bool Delete(T entity);
+        Task<bool> Delete(T entity);
         Task<bool> UpdateAsync(T entity);
 
     }
