f04f0dad82646a04403cf6d7b4114c9224ae0a9f
Павел Новопашин <95532839+MolderrOff@users.noreply.github.com>
2025-08-14 22:49:44 +0500

    Update WorkConroller.cs
    Del comments

--- WebApplication3/Controllers/WorkConroller.cs
+++ WebApplication3/Controllers/WorkConroller.cs
@@ -20,54 +20,44 @@ namespace Itmytask.Controllers
         }
 
 
-        [HttpGet] //атрибут на метод действия
+        [HttpGet]
         public async Task<IActionResult> GetWorks()
         {
-            //var response = await _workRepository.Select(); //было var response = await _carRepository.Select();
-            //var response1 =  await _workRepository.GetByNameAsync("BMWX5");//var response1 = await _carRepository.GetByName("BMWX5");
-            //var response2 = await _workRepository.GetAsync(3);// var response2 =  _carRepository.GetAsync(3);
-            //var responseSelect = db.Companies.FromSqlRaw("SELECT * FROM Companies").ToList();
-            //var responseSelect = await _carRepository.GetAsyncSelect(); //----->> 150824
+            
 
             var work = new Work()
             {
-                NameTask = "Восстановить ПК продавца",   //он зависит от car.cs в енампе (перечислении)
+                NameTask = "Восстановить ПК продавца",   
                 TaskNumber = 3928,
                 Description = "не запускается компьютер продавца",
                 Customer = "Линии любви",
                 AdressTask = "г. Орёл, ул. Революции, 38",
-                Price = 1500,
-                //StatusTask = "в работе",
-                //DateCreate = DateTime.Now,
+                Price = 1500,                
                 TypeWork = TypeWork.Free
             };
 
 
-            await _workRepository.Create(work); //var response3 =  await _carRepository.Create(car);
-                                              //await _carRepository.Delete(car);
+            await _workRepository.Create(work); 
 
             var work1 = new Work()
             {
 
-                NameTask = "Подключить МФУ",   //он зависит от car.cs в енампе (перечислении)
+                NameTask = "Подключить МФУ",   
                 TaskNumber = 3029,
                 Description = "Подключить новый МФУ на рабочее место заведующиго",
                 Customer = "Линии любви",
                 AdressTask = "г. Ульяновск, ул. Республики, 8",
                 Price = 1900,
-                //StatusTask = "в работе",
-                //DateCreate = DateTime.Now,
+                
                 TypeWork = TypeWork.Free
             };
 
 
             await _workRepository.Create(work1);
-            //await _carRepository.Delete(car1);
-
-            //var responseSelect = await _workRepository.GetAsyncSelect(); //убрал MY SQL
+           
             var responseSelect = await _workRepository.Select();
 
-            return View(responseSelect); //response будт храниться писок объектов из нашей бд
+            return View(responseSelect); 
             
         }
     }
