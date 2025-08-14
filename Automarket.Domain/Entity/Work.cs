9768c6a891ee3a651a67a50a00d4025c1cce0de6
Павел Новопашин <95532839+MolderrOff@users.noreply.github.com>
2025-08-14 22:53:51 +0500

    Update Work.cs
    Del comments

--- Automarket.Domain/Entity/Work.cs
+++ Automarket.Domain/Entity/Work.cs
@@ -10,17 +10,16 @@ namespace Itmytask.Domain.Entity
     public class Work
     {
         public int Id { get; set; }
-                //public string Name { get; set; }
+                
         public string NameTask { get; set; }  //имя заявки
         public int TaskNumber { get; set; } //номер заявки
         public string Description { get; set; } //описание заявки
-             //public string Model { get; set; }
+             
         public string Customer {  get; set; } //заказчик
-             //public double  Speed { get; set; }
+            
         public string AdressTask { get; set; } //адрес выполнения заявки
         public decimal  Price { get; set; }
-            //public string StatusTask { get; set; } //статус заявки
-        //public DateTime DateCreate { get; set; }      
+            
         public TypeWork TypeWork { get; set; } //статус заявки
 
 
